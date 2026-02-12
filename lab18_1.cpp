9ad8b7fa025e274307f499ceab682f7645f074e4
Thada <skill11888@gmail.com>
2026-02-13 05:05:26 +0700

    lab18_1 completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,20 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1,Rect R2){
+	double overlapX,overlapY,overlapR,overlapB;
+	double R1r = R1.x+R1.w;
+	double R2r = R2.x+R2.w;
+	double R1b = R1.y-R1.h;
+	double R2b = R2.y-R2.h; 
+	if (R1.x>R2.x) overlapX = R1.x;
+	else overlapX = R2.x;
+	if (R1.y<R2.y) overlapY = R1.y;
+	else overlapY = R2.y;
+	if (R1r<R2r) overlapR = R1r;
+	else overlapR = R2r;
+	if (R1b>R2b) overlapB = R1b;
+	else overlapB = R2b;
+	if (overlapR-overlapX <= 0 || overlapY-overlapB <= 0) return 0;
+	else return (overlapR-overlapX)*(overlapY-overlapB);
 }
